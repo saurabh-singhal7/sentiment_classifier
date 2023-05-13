346a145160d6b69678438a06f0c37575ca854a6f
Saurabh <saurabh7897100111@gmail.com>
2023-05-13 23:35:24 +0530

    pushing the word embeddings

--- utils.py
+++ utils.py
@@ -1,4 +1,29 @@
 import numpy as np
+import re
+
+def depure_data(data):
+    #Removing URLs with a regular expression
+    url_pattern = re.compile(r'https?://\S+|www\.\S+')
+    data = url_pattern.sub(r'', data)
+
+    # Remove Emails
+    data = re.sub('\S*@\S*\s?', '', data)
+
+    # Remove new line characters
+    data = re.sub('\s+', ' ', data)
+
+    # Remove distracting single quotes
+    data = re.sub("\'", "", data)
+        
+    data = data.strip()
+        
+    return data
+
+def remove_punctuation(text, regular_punct):
+    for punc in regular_punct:
+        if punc in text:
+            text = text.replace(punc,'')
+    return text.strip()
 
 def read_glove_vecs(glove_file):
     with open(glove_file, 'r') as f:
@@ -18,3 +43,12 @@ def read_glove_vecs(glove_file):
             index_to_words[i] = w
             i = i + 1
     return words_to_index, index_to_words, word_to_vec_map
+
+def output_to_sentiment(model_output):
+    if model_output == 0:
+        return "positive"
+    elif model_output == 1:
+        return "negative"
+    else:
+        return "neutral"
+
